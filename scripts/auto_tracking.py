f519a7528a964618deb2b150f47463b2ccc9a08f
Mikl095 <zendugan95@gmail.com>
2025-11-16 20:21:33 +0100

    Delete scripts directory

--- scripts/auto_tracking.py
+++
@@ -1,9 +0,0 @@
-from utils.storage import load_routes, save_routes
-from utils.tracking import simulate_auto_tracking
-
-routes = load_routes()
-
-for route in routes:
-    simulate_auto_tracking(route)
-
-save_routes(routes)
