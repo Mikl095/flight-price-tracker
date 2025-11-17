d314b1038e9affc09fe297f58b120eeda3c76570
Mikl095 <zendugan95@gmail.com>
2025-11-17 01:32:46 +0100

    Update email_utils.py

--- email_utils.py
+++ email_utils.py
@@ -1,25 +1,30 @@
 import os
 import requests
 
-SENDGRID_API_KEY = os.environ["SENDGRID_API_KEY"]
+API_KEY = os.environ.get("SENDGRID_API_KEY")
 
 def send_email(to_email, subject, message):
+    if not API_KEY:
+        print("❌ ERREUR : SENDGRID_API_KEY manquant dans GitHub Secrets")
+        return
+
     url = "https://api.sendgrid.com/v3/mail/send"
 
     data = {
         "personalizations": [
             {"to": [{"email": to_email}], "subject": subject}
         ],
-        "from": {"email": "alert@flight-tracker.com"},
+        "from": {"email": "alerts@flight-tracker.com"},
         "content": [
             {"type": "text/plain", "value": message}
         ],
     }
 
     headers = {
-        "Authorization": f"Bearer {SENDGRID_API_KEY}",
+        "Authorization": f"Bearer {API_KEY}",
         "Content-Type": "application/json"
     }
 
     r = requests.post(url, json=data, headers=headers)
+    print("Email status:", r.status_code)
     return r.status_code
