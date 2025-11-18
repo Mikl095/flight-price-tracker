8a58083dbf72ef60bb01cde170571220004d2267
Mikl095 <zendugan95@gmail.com>
2025-11-18 13:26:31 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,27 @@
 # app.py
 import streamlit as st
-from utils.storage import load_routes, load_email_config
+from datetime import datetime, date, timedelta
+import uuid
+import random
+import pandas as pd
 
 from ui_components import (
     render_top_bar,
     render_dashboard,
     render_add_tab,
-    render_edit_route,
     render_search_tab,
     render_config_tab
+)
+from utils.storage import (
+    ensure_data_file,
+    load_routes,
+    save_routes,
+    load_email_config,
+    save_email_config,
+    append_log,
+    count_updates_last_24h,
+    ensure_route_fields,
+    sanitize_dict
 )
 
 # Chargement des données
